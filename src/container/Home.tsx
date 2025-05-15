a995c74988131ef1b35de41b461a87732c0b4710
Juan Tapias <juantapias@Mac.lan>
2025-05-15 00:03:39 -0500

    update home

--- src/container/Home.tsx
+++ src/container/Home.tsx
@@ -1,5 +1,3 @@
-import { useEffect, useState } from "react";
-import Loading from "../components/ui/loading";
 import MainBanner from "../components/banner/main-banner/MainBanner";
 import ContentBlock from "../components/content-block/ContentBlock";
 import Rooms from "../components/rooms/Rooms";
