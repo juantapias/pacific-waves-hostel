ab5b9493e34d0ecbf997f356e1cb6b45872cbff3
Juan Tapias <juantapias@Mac.lan>
2025-05-30 10:45:31 -0500

    vercel adapters

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,11 @@
 import { defineConfig } from "astro/config";
 
 import react from "@astrojs/react";
-import node from "@astrojs/node";
+
+import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
-  adapter: node({
-    mode: "standalone",
-  }),
+  adapter: vercel(),
   integrations: [react()], // Si usas React
 });
